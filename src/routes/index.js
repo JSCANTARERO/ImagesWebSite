2704763bc45b52d76d9e03859876aa6601cae1a7
Jorge Sanchez <js7170206@gmail.com>
2021-01-05 21:19:09 -0600

    Creation of Image model with mongoose

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const {Router} = require('express');
 const router = Router();
 
+const Image = require('../models/Image');
+
 router.get('/', (req, res) => {
     res.send('Index page');
 });
@@ -9,11 +11,23 @@ router.get('/upload', (req, res) => {
     res.render('upload');
 });
 
-router.post('/upload', (req, res) => {
-    console.log(req.file); 
+router.post('/upload', async (req, res) => {
+    const image = new Image();
+    image.title = req.body.title;
+    image.description = req.body.description;
+    image.filename = req.file.filename;
+    image.path = '/img/uploads/' + req.file.filename;
+    image.originalname = req.file.originalname;
+    image.mimetype = req.file.mimetype;
+    image.size = req.file.size;
+
+    console.log(image); 
     //Cuando multer sube una imagen este la coloca en la carpeta que public pero ademas
     //hace la informacion de la imagen disponible a traves del req.file
-    res.send('Uploaded');
+
+    await image.save();
+
+    res.redirect('/');
 });
 
 router.get('/image/:id', (req, res) =>{
